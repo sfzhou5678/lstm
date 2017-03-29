51db9dd6ad6bb0500a0da4bf94798e0bb3eb5eb0
sfzhou567hp <260174969@qq.com>
2017-03-29 22:59:39 +0800

    加入统计各类命中情况的工具

--- testcode/display_dic_details.py
+++ testcode/display_dic_details.py
@@ -7,6 +7,8 @@ with open('../data/testres/id_to_word.txt','r') as f:
     id_to_word=json.loads(line)
 
 def display_detail(filepath):
+
+    influence_dic={}
     with open(filepath,'r') as f:
         total_dic=json.loads(f.readline().strip())
         hit_dic=json.loads(f.readline().strip())
@@ -24,7 +26,8 @@ def display_detail(filepath):
             if(k in stop_words):
                 continue
             # if(total_dic[k]>=20 and v > 0 and v<1):
-            #     print('%s  %.3f  %d'%(id_to_word[k],v,total_dic[k]))
+            # print('%s  %.3f  %d'%(id_to_word[k],v,total_dic[k]))
+            influence_dic[k]=float(float(1.0-v)*1.0*total_dic[k])
                 # print(str(id_to_word[k])+' , '+str(v)+" , "+str(total_dic[k]))
             if v >0:
                 total_sum+=total_dic[k]
@@ -37,10 +40,19 @@ def display_detail(filepath):
         print(hit_sum)
         print(hit_sum/total_tokens)
 
+        influence_dic=sorted(influence_dic.items(), key=lambda x:x[1],reverse=True)
+        sum=0
+        for (k,v) in influence_dic:
+            print('%s  %d'%(id_to_word[k],v))
+            sum+=v
+            if sum>int(0.9*(total_tokens-hit_sum)):
+                break
+        # print(influence_dic)
+
 filePath=r'../data/testres/total_res_dic.txt'
 
-print("============display total===================")
-display_detail(filePath)
+# print("============display total===================")
+# display_detail(filePath)
 
 nt_filepath=r'../data/testres/nonterminal_res_dic.txt'
 t_filepath=r'../data/testres/terminal_res_dic.txt'
@@ -48,5 +60,5 @@ t_filepath=r'../data/testres/terminal_res_dic.txt'
 print("============display nt===================")
 display_detail(nt_filepath)
 
-print("============display t===================")
-display_detail(t_filepath)
+# print("============display t===================")
+# display_detail(t_filepath)
